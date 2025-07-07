6d626bfe6664ffba789b9fcf01c8ff03b61a08ce
Richard Malca <rmalcac16_1@unc.edu.pe>
2025-07-07 16:04:11 -0500

    Agregar métodos de acceso y modificación para el atributo 'name' en el modelo Genre

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -9,4 +9,14 @@ class Genre extends Model
 {
     protected $fillable = ['name', 'slug', 'name_mal'];
     public $timestamps = false;
+
+    public function getNameAttribute()
+    {
+        return $this->title;
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+    }
 }
